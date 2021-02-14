74e0558c2c29f19972f382410102c9d3aad13028
sigriswil <baekseongsa@naver.com>
2021-02-14 15:18:53 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/restaurant/eatenjoy/EatEnjoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.eatenjoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatEnjoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
