7c676a89a022f687b30fb5573e070071a660a3c2
sigriswil <baekseongsa@naver.com>
2021-02-24 21:39:53 +0900

    refactor: HTTP 응답코드 어노테이션으로 변경

--- src/main/java/com/restaurant/eatenjoy/controller/UserController.java
+++ src/main/java/com/restaurant/eatenjoy/controller/UserController.java
@@ -3,10 +3,10 @@ package com.restaurant.eatenjoy.controller;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.restaurant.eatenjoy.dto.UserDto;
@@ -22,9 +22,9 @@ public class UserController {
 	private final UserService userService;
 
 	@PostMapping
-	public ResponseEntity<?> register(@RequestBody @Valid UserDto userDto) {
+	@ResponseStatus(HttpStatus.CREATED)
+	public void register(@RequestBody @Valid UserDto userDto) {
 		userService.register(userDto);
-		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 
 }
