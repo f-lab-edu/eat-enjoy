4e0d5c244c8b4e14bd050b9710913df966d8f5fe
sigriswil <baekseongsa@naver.com>
2021-02-27 00:16:23 +0900

    feat: 로그인 아이디 & 비밀번호 검증 기능 추가

--- src/main/java/com/restaurant/eatenjoy/service/UserService.java
+++ src/main/java/com/restaurant/eatenjoy/service/UserService.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.restaurant.eatenjoy.dao.UserDao;
+import com.restaurant.eatenjoy.dto.LoginDto;
 import com.restaurant.eatenjoy.dto.UserDto;
 import com.restaurant.eatenjoy.exception.DuplicateValueException;
+import com.restaurant.eatenjoy.exception.UserNotFoundException;
 import com.restaurant.eatenjoy.util.encrypt.Encryptable;
 
 import lombok.RequiredArgsConstructor;
@@ -36,6 +38,17 @@ public class UserService {
 
 		if (userDao.existsByEmail(userDto.getEmail())) {
 			throw new DuplicateValueException("이메일 주소가 이미 존재합니다.");
+		}
+	}
+
+	public void validateLoginIdAndPassword(LoginDto loginDto) {
+		loginDto = LoginDto.builder()
+			.loginId(loginDto.getLoginId())
+			.password(encryptable.encrypt(loginDto.getPassword()))
+			.build();
+
+		if (!userDao.existsByLoginIdAndPassword(loginDto)) {
+			throw new UserNotFoundException("아이디 또는 비밀번호가 일치하지 않습니다.");
 		}
 	}
 
