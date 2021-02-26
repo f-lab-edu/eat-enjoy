94f1670c3c88bb4bb82e3d7b5c6aad88a4759492
sigriswil <baekseongsa@naver.com>
2021-02-27 00:23:17 +0900

    feat: 로그인 & 로그아웃 기능 구현

---
+++ src/main/java/com/restaurant/eatenjoy/service/SessionLoginService.java
@@ -0,0 +1,37 @@
+package com.restaurant.eatenjoy.service;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Service;
+
+import com.restaurant.eatenjoy.dto.LoginDto;
+import com.restaurant.eatenjoy.exception.DuplicateValueException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SessionLoginService implements LoginService {
+
+	private static final String LOGIN_ID = "loginId";
+
+	private final HttpSession httpSession;
+
+	private final UserService userService;
+
+	@Override
+	public void login(LoginDto loginDto) {
+		if (httpSession.getAttribute(LOGIN_ID) != null) {
+			throw new DuplicateValueException("이미 로그인이 되어 있습니다.");
+		}
+
+		userService.validateLoginIdAndPassword(loginDto);
+		httpSession.setAttribute(LOGIN_ID, loginDto.getLoginId());
+	}
+
+	@Override
+	public void logout() {
+		httpSession.invalidate();
+	}
+
+}
