aecc7fedd5c3cfa17fb4d896816476b2033f393e
sjhello <sjhello235@gmail.com>
2021-03-02 15:51:34 +0900

    refactor: junit4 중복선언 제거

--- src/main/java/com/restaurant/eatenjoy/dto/UserDto.java
+++ src/main/java/com/restaurant/eatenjoy/dto/UserDto.java
@@ -10,9 +10,11 @@ import org.hibernate.validator.constraints.Length;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class UserDto {
 
 	private Long id;
--- src/test/java/com/restaurant/eatenjoy/controller/UserControllerTest.java
+++ src/test/java/com/restaurant/eatenjoy/controller/UserControllerTest.java
@@ -5,22 +5,21 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.restaurant.eatenjoy.dto.UserDto;
 import com.restaurant.eatenjoy.service.UserService;
 
-@RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest
 public class UserControllerTest {
@@ -34,34 +33,34 @@ public class UserControllerTest {
 	@Autowired
 	UserService userService;
 
+	UserDto userDto;
+
 	@BeforeEach
-	UserDto userCreate() {
-		UserDto userDto = UserDto.builder()
+	void userCreate() {
+		userDto = UserDto.builder()
 			.loginId("test")
 			.password("test123")
 			.email("test@gmail.com")
 			.regionCD("cod")
 			.build();
-
-		return userDto;
 	}
 
 	@Test
 	@DisplayName("중복된 아이디가 존재한다면 회원 가입 할 수 없다")
 	public void duplicatedId() throws Exception {
-		assertThat(userService.isDuplicatedId(userCreate().getLoginId())).isEqualTo(false);
+		assertThat(userService.isDuplicatedId(userDto.getLoginId())).isEqualTo(false);
 	}
 
 	@Test
 	@DisplayName("중복된 이메일이 존재한다면 회원 가입 할 수 없다")
 	public void duplicatedEmail() throws Exception {
-		assertThat(userService.isDuplicatedEmail(userCreate().getEmail())).isEqualTo(false);
+		assertThat(userService.isDuplicatedEmail(userDto.getEmail())).isEqualTo(false);
 	}
 
 	@Test
-	@DisplayName("중복된 아이디와 이메일이 없다면 회원가입 성공")
 	public void createUsers() throws Exception {
-		String json = objectMapper.writeValueAsString(userCreate());
+
+		String json = objectMapper.writeValueAsString(userDto);
 
 		this.mockMvc.perform(post("/api/users")
 			.content(json)
