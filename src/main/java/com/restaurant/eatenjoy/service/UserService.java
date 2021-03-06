398862b3366a89f19596ae9971846b87b38edbbd
sigriswil <baekseongsa@naver.com>
2021-03-07 02:26:10 +0900

    feat: 회원가입 시 인증 토큰 발행 후 인증 메일 전송 기능 추가

--- src/main/java/com/restaurant/eatenjoy/service/UserService.java
+++ src/main/java/com/restaurant/eatenjoy/service/UserService.java
@@ -1,5 +1,7 @@
 package com.restaurant.eatenjoy.service;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,6 +11,8 @@ import com.restaurant.eatenjoy.dto.UserDto;
 import com.restaurant.eatenjoy.exception.DuplicateValueException;
 import com.restaurant.eatenjoy.exception.UserNotFoundException;
 import com.restaurant.eatenjoy.util.encrypt.Encryptable;
+import com.restaurant.eatenjoy.util.mail.MailService;
+import com.restaurant.eatenjoy.util.mail.MailMessage;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,15 +24,22 @@ public class UserService {
 
 	private final Encryptable encryptable;
 
+	private final MailService emailService;
+
 	@Transactional
 	public void register(UserDto userDto) {
 		validateLoginIdAndEmail(userDto);
-		userDao.register(UserDto.builder()
+
+		userDto = UserDto.builder()
 			.loginId(userDto.getLoginId())
 			.password(encryptable.encrypt(userDto.getPassword()))
 			.email(userDto.getEmail())
+			.emailToken(UUID.randomUUID().toString())
 			.regionCd(userDto.getRegionCd())
-			.build());
+			.build();
+		userDao.register(userDto);
+
+		sendCertificationMail(userDto);
 	}
 
 	private void validateLoginIdAndEmail(UserDto userDto) {
@@ -39,6 +50,15 @@ public class UserService {
 		if (userDao.existsByEmail(userDto.getEmail())) {
 			throw new DuplicateValueException("이메일 주소가 이미 존재합니다.");
 		}
+	}
+
+	private void sendCertificationMail(UserDto userDto) {
+		emailService.send(MailMessage.builder()
+			.loginId(userDto.getLoginId())
+			.to(userDto.getEmail())
+			.subject("eat-enjoy, 회원가입 인증")
+			.token(userDto.getEmailToken())
+			.build());
 	}
 
 	public void validateLoginIdAndPassword(LoginDto loginDto) {
