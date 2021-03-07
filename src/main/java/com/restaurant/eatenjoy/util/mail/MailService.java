89f462a5c81695e62e9c3dfbe86e35c486d98cb8
sigriswil <baekseongsa@naver.com>
2021-03-07 18:31:53 +0900

    feat: 인증 토큰 관련 상수 값 설정

--- src/main/java/com/restaurant/eatenjoy/util/mail/MailService.java
+++ src/main/java/com/restaurant/eatenjoy/util/mail/MailService.java
@@ -1,6 +1,12 @@
 package com.restaurant.eatenjoy.util.mail;
 
+import java.time.Duration;
+
 public interface MailService {
+
+	String MAIL_TOKEN_PREFIX_KEY = "mail:token:";
+
+	Duration MAIL_TOKEN_TIMEOUT_SECOND = Duration.ofSeconds(86400);
 
 	void send(MailMessage mailMessage);
 
