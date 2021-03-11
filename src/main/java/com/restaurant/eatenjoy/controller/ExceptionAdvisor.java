ef9148999a7242ec8c6461681a0f292ca7487da5
sigriswil <baekseongsa@naver.com>
2021-03-11 23:40:19 +0900

    feat: AuthorizationException ExceptionHandler 적용

--- src/main/java/com/restaurant/eatenjoy/controller/ExceptionAdvisor.java
+++ src/main/java/com/restaurant/eatenjoy/controller/ExceptionAdvisor.java
@@ -5,12 +5,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.restaurant.eatenjoy.exception.AlreadyCertifiedException;
+import com.restaurant.eatenjoy.exception.AuthorizationException;
 import com.restaurant.eatenjoy.exception.DuplicateValueException;
 import com.restaurant.eatenjoy.exception.NotFoundException;
 import com.restaurant.eatenjoy.exception.UnauthorizedException;
@@ -48,6 +50,12 @@ public class ExceptionAdvisor {
 	@ExceptionHandler(UnauthorizedException.class)
 	public ResponseEntity<String> processUnauthorizedError() {
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("요청할 권한이 없습니다. 로그인 후 요청하시기 바랍니다.");
+	}
+
+	@ExceptionHandler(AuthorizationException.class)
+	public ResponseEntity<String> processAuthorizationError(AuthorizationException exception) {
+		String message = StringUtils.hasLength(exception.getMessage()) ? exception.getMessage() : "해당 리소스에 접근할 수 없습니다.";
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(message);
 	}
 
 	@Getter
