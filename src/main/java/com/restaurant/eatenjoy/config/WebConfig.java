fb4bce6de163b2805d03392f6c49a80b1760491d
sigriswil <baekseongsa@naver.com>
2021-03-11 23:39:09 +0900

    feat: 권한 검증 인터셉터 구현 및 적용

--- src/main/java/com/restaurant/eatenjoy/config/WebConfig.java
+++ src/main/java/com/restaurant/eatenjoy/config/WebConfig.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.restaurant.eatenjoy.util.interceptor.AuthorityInterceptor;
 import com.restaurant.eatenjoy.util.resolver.CurrentLoginIdArgumentResolver;
 
 import lombok.RequiredArgsConstructor;
@@ -16,9 +18,16 @@ public class WebConfig implements WebMvcConfigurer {
 
 	private final CurrentLoginIdArgumentResolver currentLoginIdArgumentResolver;
 
+	private final AuthorityInterceptor authorityInterceptor;
+
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(currentLoginIdArgumentResolver);
 	}
 
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authorityInterceptor)
+			.excludePathPatterns("/api/*/login", "/api/*/logout", "/api/users/check-mail-token");
+	}
 }
