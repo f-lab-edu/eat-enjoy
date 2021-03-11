501cf0d3c220dc001aa887807a1ef4eede267ffa
sigriswil <baekseongsa@naver.com>
2021-03-12 01:35:09 +0900

    test: 메일 관련 기능 및 탈퇴 테스트 코드 작성

--- src/test/java/com/restaurant/eatenjoy/service/UserServiceTest.java
+++ src/test/java/com/restaurant/eatenjoy/service/UserServiceTest.java
@@ -3,6 +3,8 @@ package com.restaurant.eatenjoy.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,21 +13,34 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.restaurant.eatenjoy.dao.MailTokenDao;
 import com.restaurant.eatenjoy.dao.UserDao;
 import com.restaurant.eatenjoy.dto.LoginDto;
 import com.restaurant.eatenjoy.dto.UserDto;
+import com.restaurant.eatenjoy.exception.AlreadyCertifiedException;
 import com.restaurant.eatenjoy.exception.DuplicateValueException;
+import com.restaurant.eatenjoy.exception.MailTokenNotFoundException;
+import com.restaurant.eatenjoy.exception.NoMatchedPasswordException;
 import com.restaurant.eatenjoy.exception.UserNotFoundException;
 import com.restaurant.eatenjoy.util.encrypt.Encryptable;
+import com.restaurant.eatenjoy.util.mail.MailService;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
+	public static final String TEST_MAIL = "test@test.com";
+	
 	@Mock
 	private UserDao userDao;
 
 	@Mock
 	private Encryptable encryptable;
+
+	@Mock
+	private MailService mailService;
+
+	@Mock
+	private MailTokenDao mailTokenDao;
 
 	@InjectMocks
 	private UserService userService;
@@ -37,7 +52,7 @@ class UserServiceTest {
 		userDto = UserDto.builder()
 			.loginId("test")
 			.password("1234")
-			.email("test@test.com")
+			.email(TEST_MAIL)
 			.regionCd("001")
 			.build();
 	}
@@ -53,49 +68,136 @@ class UserServiceTest {
 	@Test
 	@DisplayName("중복된 이메일은 회원가입을 할 수 없다.")
 	void failToRegisterEmailDuplicated() {
-		given(userDao.existsByEmail("test@test.com")).willReturn(true);
+		given(userDao.existsByEmail(TEST_MAIL)).willReturn(true);
 		assertThatThrownBy(() -> userService.register(userDto)).isInstanceOf(DuplicateValueException.class);
-		then(userDao).should(times(1)).existsByEmail("test@test.com");
+		then(userDao).should(times(1)).existsByEmail(TEST_MAIL);
 	}
 
 	@Test
-	@DisplayName("로그인 아이디와 이메일이 중복되지 않으면 회원가입을 성공한다.")
-	void successToRegister() {
+	@DisplayName("로그인 아이디와 이메일이 중복되지 않으면 회원가입을 성공하고 인증메일을 전송한다.")
+	void successToRegisterAndSendMail() {
 		given(userDao.existsByLoginId("test")).willReturn(false);
-		given(userDao.existsByEmail("test@test.com")).willReturn(false);
+		given(userDao.existsByEmail(TEST_MAIL)).willReturn(false);
 		given(encryptable.encrypt("1234")).willReturn("!@#$%^&*()");
 
 		userService.register(userDto);
 
 		then(userDao).should(times(1)).existsByLoginId("test");
-		then(userDao).should(times(1)).existsByEmail("test@test.com");
+		then(userDao).should(times(1)).existsByEmail(TEST_MAIL);
 		then(encryptable).should(times(1)).encrypt("1234");
+		then(mailService).should(times(1)).send(any());
+		then(mailTokenDao).should(times(1)).create(eq(TEST_MAIL), any(), eq(Duration.ofSeconds(86400)));
 	}
 
 	@Test
 	@DisplayName("로그인 정보로 사용자를 찾지 못하면 UserNotFoundException 예외가 발생한다.")
 	void failToLoginUserNotFound() {
-		given(userDao.existsByLoginIdAndPassword("test", any())).willReturn(false);
+		given(userDao.existsByLoginIdAndPassword(eq("test"), any())).willReturn(false);
 
 		assertThatThrownBy(() -> userService.validateLoginIdAndPassword(LoginDto.builder()
 			.loginId("test")
 			.password("1111")
 			.build())).isInstanceOf(UserNotFoundException.class);
 
-		then(userDao).should(times(1)).existsByLoginIdAndPassword("test", any());
+		then(userDao).should(times(1)).existsByLoginIdAndPassword(eq("test"), any());
 	}
 
 	@Test
 	@DisplayName("로그인 정보로 사용자가 존재하면 정상이다.")
 	void normalToLoginUserFound() {
-		given(userDao.existsByLoginIdAndPassword("test", any())).willReturn(true);
+		given(userDao.existsByLoginIdAndPassword(eq("test"), any())).willReturn(true);
 
 		userService.validateLoginIdAndPassword(LoginDto.builder()
 			.loginId("test")
 			.password("1234")
 			.build());
 
-		then(userDao).should(times(1)).existsByLoginIdAndPassword("test", any());
+		then(userDao).should(times(1)).existsByLoginIdAndPassword(eq("test"), any());
+	}
+
+	@Test
+	@DisplayName("메일로 사용자를 찾을 수 없으면 메일 인증에 실패한다.")
+	void failToCertifyEmailTokenUserNotFound() {
+		given(userDao.existsByEmail(TEST_MAIL)).willReturn(false);
+		assertThatThrownBy(() -> userService.certifyEmailToken(TEST_MAIL, "1234"))
+			.isInstanceOf(UserNotFoundException.class);
+		then(userDao).should(times(1)).existsByEmail(TEST_MAIL);
+	}
+
+	@Test
+	@DisplayName("인증 토큰이 일치하지 않으면 메일 인증에 실패한다.")
+	void failToCertifyEmailTokenNotMatch() {
+		given(userDao.existsByEmail(TEST_MAIL)).willReturn(true);
+		given(mailTokenDao.findByMail(TEST_MAIL)).willReturn("1111");
+
+		assertThatThrownBy(() -> userService.certifyEmailToken(TEST_MAIL, "1234"))
+			.isInstanceOf(MailTokenNotFoundException.class);
+
+		then(userDao).should(times(1)).existsByEmail(TEST_MAIL);
+		then(mailTokenDao).should(times(1)).findByMail(TEST_MAIL);
+	}
+
+	@Test
+	@DisplayName("메일 인증에 성공한다.")
+	void successToCertifyEmailToken() {
+		given(userDao.existsByEmail(TEST_MAIL)).willReturn(true);
+		given(mailTokenDao.findByMail(TEST_MAIL)).willReturn("1234");
+
+		userService.certifyEmailToken(TEST_MAIL, "1234");
+
+		then(userDao).should(times(1)).existsByEmail(TEST_MAIL);
+		then(mailTokenDao).should(times(1)).findByMail(TEST_MAIL);
+		then(userDao).should(times(1)).updateEmailCertified(TEST_MAIL);
+	}
+
+	@Test
+	@DisplayName("인증을 완료하면 인증 메일을 재전송할 수 없다.")
+	void failToResendMailCertified() {
+		userDto = UserDto.builder()
+			.loginId("test")
+			.certified(true)
+			.build();
+
+		given(userDao.findByLoginId("test")).willReturn(userDto);
+
+		assertThatThrownBy(() -> userService.resendCertificationMail("test"))
+			.isInstanceOf(AlreadyCertifiedException.class);
+
+		then(userDao).should(times(1)).findByLoginId("test");
+	}
+
+	@Test
+	@DisplayName("인증 메일을 재전송한다.")
+	void resendCertificationMail() {
+		userDto = UserDto.builder()
+			.loginId("test")
+			.email(TEST_MAIL)
+			.certified(false)
+			.build();
+
+		given(userDao.findByLoginId("test")).willReturn(userDto);
+
+		userService.resendCertificationMail("test");
+
+		then(mailService).should(times(1)).send(any());
+		then(mailTokenDao).should(times(1)).create(eq(TEST_MAIL), any(), eq(Duration.ofSeconds(86400)));
+	}
+
+	@Test
+	@DisplayName("비밀번호가 일치하지 않으면 회원탈퇴에 실패한다.")
+	void failToMemberWithdrawalIfPasswordNotMatch() {
+		given(userDao.existsByLoginIdAndPassword(eq("test"), any())).willReturn(false);
+		assertThatThrownBy(() -> userService.delete("test", "1234"))
+			.isInstanceOf(NoMatchedPasswordException.class);
+		then(userDao).should(times(1)).existsByLoginIdAndPassword(eq("test"), any());
+	}
+
+	@Test
+	@DisplayName("비밀번호가 일치하면 회원탈퇴에 성공한다.")
+	void successToMemberWithdrawal() {
+		given(userDao.existsByLoginIdAndPassword(eq("test"), any())).willReturn(true);
+		userService.delete("test", "1234");
+		then(userDao).should(times(1)).existsByLoginIdAndPassword(eq("test"), any());
 	}
 
 }
