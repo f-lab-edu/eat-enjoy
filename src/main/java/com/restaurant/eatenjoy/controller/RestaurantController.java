fa3ab1149874d1989ffaa9cac409ff65c6c64497
sjhello <sjhello235@gmail.com>
2021-04-14 22:04:42 +0900

    add: 식당 리스트 조회

--- src/main/java/com/restaurant/eatenjoy/controller/RestaurantController.java
+++ src/main/java/com/restaurant/eatenjoy/controller/RestaurantController.java
@@ -1,8 +1,12 @@
 package com.restaurant.eatenjoy.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.restaurant.eatenjoy.annotation.Authority;
 import com.restaurant.eatenjoy.annotation.LoginAuthId;
+import com.restaurant.eatenjoy.dto.PageDto;
 import com.restaurant.eatenjoy.dto.RestaurantDto;
+import com.restaurant.eatenjoy.dto.RestaurantListDto;
 import com.restaurant.eatenjoy.service.RestaurantService;
 import com.restaurant.eatenjoy.util.security.Role;
 
@@ -20,14 +26,20 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/restaurants")
 @RequiredArgsConstructor
-@Authority(Role.OWNER)
 public class RestaurantController {
 
 	private final RestaurantService restaurantService;
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
+	@Authority(Role.OWNER)
 	public void addRestaurant(@RequestBody @Valid RestaurantDto restaurantDto, @LoginAuthId Long ownerId) {
 		restaurantService.register(restaurantDto, ownerId);
+	}
+
+	@GetMapping
+	public ResponseEntity<List<RestaurantListDto>> getRestaurantList(long lastRestaurantId) {
+		List<RestaurantListDto> list = restaurantService.getListOfRestaurant(new PageDto(lastRestaurantId));
+		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/restaurant/eatenjoy/dao/RestaurantDao.java
+++ src/main/java/com/restaurant/eatenjoy/dao/RestaurantDao.java
@@ -1,10 +1,16 @@
 package com.restaurant.eatenjoy.dao;
 
+import java.util.List;
+
+import com.restaurant.eatenjoy.dto.PageDto;
 import com.restaurant.eatenjoy.dto.RestaurantDto;
+import com.restaurant.eatenjoy.dto.RestaurantListDto;
 
 public interface RestaurantDao {
 
 	void register(RestaurantDto restaurantDto);
 
 	boolean findByBizrNo(String bizrNo);
+
+	List<RestaurantListDto> findAllRestaurantList(PageDto pageDto);
 }
