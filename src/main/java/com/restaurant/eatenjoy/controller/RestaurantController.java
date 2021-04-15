f91867db62a005ec05dd200df2e075582cc916ed
sjhello <sjhello235@gmail.com>
2021-04-16 01:50:17 +0900

    refactor: 식당 리스트 리턴 타입 변경 & 유저 권한 체크 추가

--- src/main/java/com/restaurant/eatenjoy/controller/RestaurantController.java
+++ src/main/java/com/restaurant/eatenjoy/controller/RestaurantController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,20 +25,20 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/restaurants")
 @RequiredArgsConstructor
+@Authority(Role.OWNER)
 public class RestaurantController {
 
 	private final RestaurantService restaurantService;
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	@Authority(Role.OWNER)
 	public void addRestaurant(@RequestBody @Valid RestaurantDto restaurantDto, @LoginAuthId Long ownerId) {
 		restaurantService.register(restaurantDto, ownerId);
 	}
 
 	@GetMapping
-	public ResponseEntity<List<RestaurantListDto>> getRestaurantList(long lastRestaurantId) {
-		List<RestaurantListDto> list = restaurantService.getListOfRestaurant(new PageDto(lastRestaurantId));
-		return ResponseEntity.ok().body(list);
+	@Authority(Role.USER)
+	public List<RestaurantListDto> getRestaurantList(long lastRestaurantId) {
+		return restaurantService.getListOfRestaurant(new PageDto(lastRestaurantId));
 	}
 }
