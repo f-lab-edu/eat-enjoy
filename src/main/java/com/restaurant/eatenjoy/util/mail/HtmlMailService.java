c6a4b7a551a61eacb8cdb47bf53ee8e3a2bd9422
sigriswil <baekseongsa@naver.com>
2021-05-03 23:34:28 +0900

    refactor: html 메일 양식 html 파일로 분리

--- src/main/java/com/restaurant/eatenjoy/util/mail/HtmlMailService.java
+++ src/main/java/com/restaurant/eatenjoy/util/mail/HtmlMailService.java
@@ -1,40 +1,43 @@
 package com.restaurant.eatenjoy.util.mail;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import com.restaurant.eatenjoy.exception.MailSendFailedException;
+import com.restaurant.eatenjoy.exception.NotFoundException;
 import com.restaurant.eatenjoy.util.security.Role;
 
 import lombok.RequiredArgsConstructor;
 
+@Profile("dev")
 @Component
 @RequiredArgsConstructor
 public class HtmlMailService implements MailService {
 
-	private static final String HTML_CONTENT = initializeHtmlContent();
+	private static final String HTML_CONTENT;
+
+	static {
+		ClassPathResource resource = new ClassPathResource("static/welcome.html");
+		try {
+			Path path = Paths.get(resource.getURI());
+			HTML_CONTENT = String.join("\n", Files.readAllLines(path));
+		} catch (IOException e) {
+			throw new NotFoundException("메일 양식을 찾을 수 없습니다.");
+		}
+	}
 
 	private final JavaMailSender javaMailSender;
-
-	private static String initializeHtmlContent() {
-		return "<!DOCTYPE html>\n<html>\n<head></head>\n<body>\n"
-			+ "<div style=\"font-family: 'Apple SD Gothic Neo', 'sans-serif' !important; width: 450px; border-top: 4px solid #02b875; margin: 10px auto; box-sizing: border-box;\">"
-			+ "	<h1 style=\"margin: 0; padding: 0 5px; font-size: 28px; font-weight: 400;\">"
-			+ "		<span style=\"color: #02b875\">메일인증</span> 안내입니다.</h1>\n"
-			+ "	<p style=\"font-size: 16px; line-height: 26px; margin-top: 50px; padding: 0 5px;\">"
-			+ "		<b>LOGIN_ID</b>님 안녕하세요.<br />"
-			+ "		CONTENT"
-			+ "		아래 <b style=\"color: #02b875\">'메일 인증'</b> 버튼을 클릭하여 인증을 완료해 주세요.<br />"
-			+ "		감사합니다.</p>"
-			+ "	<a style=\"color: #FFF; text-decoration: none; text-align: center;\""
-			+ "	href=\"http://localhost:8080/api/ROLE/check-mail-token?email=EMAIL&token=TOKEN\" target=\"_blank\">"
-			+ "		<p style=\"display: inline-block; width: 210px; height: 45px; margin: 30px 5px 40px; background: #02b875; line-height: 45px; vertical-align: middle; font-size: 16px;\">메일 인증</p></a>"
-			+ "	<div style=\"border-top: 1px solid #DDD; padding: 5px;\"></div>\n</div>\n</body>\n</html>";
-	}
 
 	@Override
 	public void send(MailMessage mailMessage) {
