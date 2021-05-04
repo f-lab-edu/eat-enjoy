48fe02cd983fa8df5d4b57f9b309b296d1e726d9
sjhello <sjhello235@gmail.com>
2021-05-04 22:20:58 +0900

    add & update: 식당 수정 쿼리 추가 & 사업자 번호 검색 쿼리 이름 변경

--- src/main/java/com/restaurant/eatenjoy/dao/RestaurantDao.java
+++ src/main/java/com/restaurant/eatenjoy/dao/RestaurantDao.java
@@ -5,14 +5,17 @@ import java.util.List;
 import com.restaurant.eatenjoy.dto.RestaurantDto;
 import com.restaurant.eatenjoy.dto.RestaurantInfo;
 import com.restaurant.eatenjoy.dto.RestaurantListDto;
+import com.restaurant.eatenjoy.dto.UpdateRestaurant;
 
 public interface RestaurantDao {
 
 	void register(RestaurantDto restaurantDto);
 
-	boolean findByBizrNo(String bizrNo);
+	boolean existByBizrNo(String bizrNo);
 
 	List<RestaurantListDto> findAllRestaurantList(Long lastRestaurantId, Long ownerId);
 
 	RestaurantInfo findById(Long id);
+
+	void modifyRestaurantInfo(UpdateRestaurant updateRestaurant);
 }
