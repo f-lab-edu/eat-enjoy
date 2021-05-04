4082831174bea600111a9f73f2b0c26e0919547d
sjhello <sjhello235@gmail.com>
2021-05-04 22:22:02 +0900

    add: 식당 수정 컨트롤러 & 서비스 추가

--- src/main/java/com/restaurant/eatenjoy/controller/RestaurantController.java
+++ src/main/java/com/restaurant/eatenjoy/controller/RestaurantController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ import com.restaurant.eatenjoy.annotation.OwnersRestaurantCheck;
 import com.restaurant.eatenjoy.dto.RestaurantDto;
 import com.restaurant.eatenjoy.dto.RestaurantInfo;
 import com.restaurant.eatenjoy.dto.RestaurantListDto;
+import com.restaurant.eatenjoy.dto.UpdateRestaurant;
 import com.restaurant.eatenjoy.exception.NotFoundException;
 import com.restaurant.eatenjoy.service.RestaurantService;
 import com.restaurant.eatenjoy.util.security.Role;
@@ -65,5 +67,16 @@ public class RestaurantController {
 	@OwnersRestaurantCheck
 	public RestaurantInfo getRestaurant(@PathVariable Long restaurantId) {
 		return restaurantService.findById(restaurantId);
+	}
+
+	/*
+	* 사장님의 식당 정보를 수정한다
+	* @param restaurantId 수정할 레스토랑의 id
+	* */
+	@PatchMapping("{restaurantId}")
+	@ResponseStatus(HttpStatus.CREATED)
+	@OwnersRestaurantCheck
+	public void updateRestaurant(@RequestBody @Valid UpdateRestaurant restaurant) {
+		restaurantService.updateRestaurant(restaurant);
 	}
 }
--- src/main/java/com/restaurant/eatenjoy/service/RestaurantService.java
+++ src/main/java/com/restaurant/eatenjoy/service/RestaurantService.java
@@ -7,17 +7,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.restaurant.eatenjoy.dao.RestaurantDao;
-import com.restaurant.eatenjoy.dto.PageDto;
 import com.restaurant.eatenjoy.dto.RestaurantDto;
 import com.restaurant.eatenjoy.dto.RestaurantInfo;
 import com.restaurant.eatenjoy.dto.RestaurantListDto;
+import com.restaurant.eatenjoy.dto.UpdateRestaurant;
 import com.restaurant.eatenjoy.exception.BizrNoValidException;
 import com.restaurant.eatenjoy.exception.DuplicateValueException;
 import com.restaurant.eatenjoy.exception.NotFoundException;
 import com.restaurant.eatenjoy.exception.RestaurantMinOrderPriceValueException;
-import com.restaurant.eatenjoy.util.bizrNoValid.BizrNoValidCheck;
+import com.restaurant.eatenjoy.util.restaurant.bizrNoValid.BizrNoValidCheck;
 
-import io.netty.util.internal.ObjectUtil;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -33,7 +32,7 @@ public class RestaurantService {
 			throw new RestaurantMinOrderPriceValueException("매장 결재 방식이 선불일 경우 최소 주문 가격이 0원이 될 순 없습니다");
 		}
 
-		if (findByBizrNo(restaurantDto.getBizrNo())) {
+		if (existByBizrNo(restaurantDto.getBizrNo())) {
 			throw new DuplicateValueException("이미 존재하는 사업자 번호입니다");
 		}
 
@@ -59,8 +58,8 @@ public class RestaurantService {
 		restaurantDao.register(restaurantDto);
 	}
 
-	public boolean findByBizrNo(String bizrNo) {
-		return restaurantDao.findByBizrNo(bizrNo);
+	public boolean existByBizrNo(String bizrNo) {
+		return restaurantDao.existByBizrNo(bizrNo);
 	}
 
 	public List<RestaurantListDto> getListOfRestaurant(Long lastRestaurantId, Long ownerId) {
@@ -77,4 +76,18 @@ public class RestaurantService {
 
 		return restaurantInfo;
 	}
+
+	public void updateRestaurant(UpdateRestaurant updateRestaurant) {
+
+		if (updateRestaurant.getPaymentType().equals("선불") && updateRestaurant.getMinOrderPrice() == 0) {
+			throw new RestaurantMinOrderPriceValueException("매장 결재 방식이 선불일 경우 최소 주문 가격이 0원이 될 순 없습니다");
+		}
+
+		if (!BizrNoValidCheck.valid(updateRestaurant.getBizrNo())) {
+			throw new BizrNoValidException("사업자 등록 번호가 잘못 되었습니다");
+		}
+
+		restaurantDao.modifyRestaurantInfo(updateRestaurant);
+	}
+
 }
