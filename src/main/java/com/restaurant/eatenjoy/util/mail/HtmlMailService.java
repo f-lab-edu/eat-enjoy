9b1b6e285090697cbdd5664e60f82c893ab37708
sigriswil <baekseongsa@naver.com>
2021-05-09 23:47:41 +0900

    refactor: 회원가입 인증 html 파일이 존재하지 않을 서버 예외 발생하도록 변경

--- src/main/java/com/restaurant/eatenjoy/util/mail/HtmlMailService.java
+++ src/main/java/com/restaurant/eatenjoy/util/mail/HtmlMailService.java
@@ -15,6 +15,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import com.restaurant.eatenjoy.exception.MailSendFailedException;
+import com.restaurant.eatenjoy.exception.NotFoundException;
 import com.restaurant.eatenjoy.util.security.Role;
 
 import lombok.RequiredArgsConstructor;
@@ -24,27 +25,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class HtmlMailService implements MailService {
 
-	private static final String HTML_CONTENT = initializeHtmlContent();
+	private static final String HTML_CONTENT;
 
-	private static String initializeHtmlContent() {
+	static {
 		ClassPathResource resource = new ClassPathResource("static/welcome.html");
 		try {
 			Path path = Paths.get(resource.getURI());
-			return String.join("\n", Files.readAllLines(path));
+			HTML_CONTENT = String.join("\n", Files.readAllLines(path));
 		} catch (IOException e) {
-			return "<!DOCTYPE html>\n<html>\n<head></head>\n<body>\n"
-				+ "<div style=\"font-family: 'Apple SD Gothic Neo', 'sans-serif' !important; width: 450px; border-top: 4px solid #02b875; margin: 10px auto; box-sizing: border-box;\">"
-				+ "	<h1 style=\"margin: 0; padding: 0 5px; font-size: 28px; font-weight: 400;\">"
-				+ "		<span style=\"color: #02b875\">메일인증</span> 안내입니다.</h1>\n"
-				+ "	<p style=\"font-size: 16px; line-height: 26px; margin-top: 50px; padding: 0 5px;\">"
-				+ "		<b>LOGIN_ID</b>님 안녕하세요.<br />"
-				+ "		CONTENT"
-				+ "		아래 <b style=\"color: #02b875\">'메일 인증'</b> 버튼을 클릭하여 인증을 완료해 주세요.<br />"
-				+ "		감사합니다.</p>"
-				+ "	<a style=\"color: #FFF; text-decoration: none; text-align: center;\""
-				+ "	href=\"CHECK_MAIL_TOKEN_URL\" target=\"_blank\">"
-				+ "		<p style=\"display: inline-block; width: 210px; height: 45px; margin: 30px 5px 40px; background: #02b875; line-height: 45px; vertical-align: middle; font-size: 16px;\">메일 인증</p></a>"
-				+ "	<div style=\"border-top: 1px solid #DDD; padding: 5px;\"></div>\n</div>\n</body>\n</html>";
+			throw new NotFoundException("메일 양식을 찾을 수 없습니다.");
 		}
 	}
 
