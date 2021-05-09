d62c9b0c6503c90e7fb4e94718e5cdcade115224
sigriswil <baekseongsa@naver.com>
2021-05-10 00:42:28 +0900

    test: 메뉴 수정 및 삭제 테스크 코드 작성

--- src/test/java/com/restaurant/eatenjoy/service/MenuServiceTest.java
+++ src/test/java/com/restaurant/eatenjoy/service/MenuServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
@@ -31,6 +32,9 @@ public class MenuServiceTest {
 
 	@Mock
 	private FileService fileService;
+
+	@Mock
+	private ApplicationEventPublisher eventPublisher;
 
 	@InjectMocks
 	private MenuService menuService;
@@ -54,41 +58,56 @@ public class MenuServiceTest {
 	void failToRegisterMenuIfSameNameAlreadyExists() {
 		doThrow(DuplicateKeyException.class).when(menuDao).register(menuDto);
 
-		assertThatThrownBy(() -> menuService.register(menuDto, null))
+		assertThatThrownBy(() -> menuService.register(menuDto))
 			.isInstanceOf(DuplicateValueException.class);
 
 		then(menuDao).should(times(1)).register(menuDto);
-		then(fileService).should(times(0)).uploadFile(null);
-		then(fileService).should(times(0)).saveFileInfo(any());
-		then(menuDao).should(times(0)).updateFileIdById(any(), any());
+		then(eventPublisher).should(times(0)).publishEvent(any());
+	}
+
+	@Test
+	@DisplayName("업로드한 파일이 있고, 같은 메뉴 이름이 이미 존재할 경우 메뉴를 등록할 수 없고 업로드된 파일은 삭제된다.")
+	void failToRegisterMenuAndDeleteFileIfSameNameAndUploadFileAlreadyExists() {
+		MenuDto menuDtoWithUploadFile = MenuDto.builder()
+			.id(1L)
+			.name("test")
+			.intrDc("테스트 메뉴 입니다.")
+			.price(1000)
+			.sort(1)
+			.menuGroupId(1L)
+			.uploadFile(FileDto.builder().id(1L).build())
+			.build();
+
+		doThrow(DuplicateKeyException.class).when(menuDao).register(menuDtoWithUploadFile);
+
+		assertThatThrownBy(() -> menuService.register(menuDtoWithUploadFile))
+			.isInstanceOf(DuplicateValueException.class);
+
+		then(menuDao).should(times(1)).register(menuDtoWithUploadFile);
+		then(eventPublisher).should(times(1)).publishEvent(menuDtoWithUploadFile.getUploadFile());
 	}
 
 	@Test
 	@DisplayName("메뉴 등록에 성공하면 데이터베이스에 성공적으로 입력된다.")
 	void successToRegisterMenu() {
-		menuService.register(menuDto, null);
+		menuService.register(menuDto);
 		then(menuDao).should(times(1)).register(menuDto);
-		then(fileService).should(times(0)).uploadFile(null);
-		then(fileService).should(times(0)).saveFileInfo(any());
-		then(menuDao).should(times(0)).updateFileIdById(any(), any());
 	}
 
 	@Test
-	@DisplayName("업로드할 파일이 이미지 파일이 아니면 메뉴 등록에 실패한다.")
-	void failToRegisterMenuIfUploadFileIsNotImage() {
+	@DisplayName("업로드할 파일이 이미지 파일이 아니면 이미지 업로드에 실패한다.")
+	void failToUploadImageIfUploadFileIsNotImage() {
 		MultipartFile multipartFile = getMockMultipartFile("text", "test.txt");
 
-		assertThatThrownBy(() -> menuService.register(menuDto, multipartFile))
+		assertThatThrownBy(() -> menuService.uploadImage(multipartFile))
 			.isInstanceOf(FileNotSupportException.class);
 
-		then(menuDao).should(times(1)).register(menuDto);
 		then(fileService).should(times(0)).uploadFile(multipartFile);
 		then(fileService).should(times(0)).saveFileInfo(any());
-		then(menuDao).should(times(0)).updateFileIdById(any(), any());
 	}
 
 	@Test
-	@DisplayName("업로드할 파일이 이미지 파일이면 메뉴 등록에 성공한다.")
+	@DisplayName("업로드할 파일이 이미지 파일이면 이미지 업로드에 성공한다.")
 	void successToRegisterMenuIfUploadFileIsImage() {
 		MultipartFile multipartFile = getMockMultipartFile("image", "image.jpg");
 
@@ -99,12 +118,36 @@ public class MenuServiceTest {
 		given(fileService.uploadFile(multipartFile)).willReturn(fileDto);
 		given(fileService.saveFileInfo(fileDto)).willReturn(1L);
 
-		menuService.register(menuDto, multipartFile);
+		menuService.uploadImage(multipartFile);
 
-		then(menuDao).should(times(1)).register(menuDto);
 		then(fileService).should(times(1)).uploadFile(multipartFile);
 		then(fileService).should(times(1)).saveFileInfo(any());
-		then(menuDao).should(times(1)).updateFileIdById(1L, 1L);
+	}
+
+	@Test
+	@DisplayName("같은 메뉴 이름이 이미 존재할 경우 메뉴를 수정할 수 없다.")
+	void fileToUpdateMenuIfSameNameAlreadyExists() {
+		UpdateMenuDto updateMenuDto = getUpdateMenuDto();
+		doThrow(DuplicateKeyException.class).when(menuDao).updateById(updateMenuDto);
+
+		assertThatThrownBy(() -> menuService.update(updateMenuDto))
+			.isInstanceOf(DuplicateValueException.class);
+
+		then(menuDao).should(times(1)).updateById(updateMenuDto);
+		then(eventPublisher).should(times(0)).publishEvent(any());
+	}
+
+	@Test
+	@DisplayName("업로드한 파일이 있고, 같은 메뉴 이름이 이미 존재할 경우 메뉴를 수정할 수 없고 업로드된 파일은 삭제된다.")
+	void failToUpdateMenuAndDeleteFileIfSameNameAndUploadFileAlreadyExists() {
+		UpdateMenuDto updateMenuDto = getUpdateMenuDtoWithFile();
+		doThrow(DuplicateKeyException.class).when(menuDao).updateById(updateMenuDto);
+
+		assertThatThrownBy(() -> menuService.update(updateMenuDto))
+			.isInstanceOf(DuplicateValueException.class);
+
+		then(menuDao).should(times(1)).updateById(updateMenuDto);
+		then(eventPublisher).should(times(1)).publishEvent(updateMenuDto.getUploadFile());
 	}
 
 	@Test
@@ -114,30 +157,11 @@ public class MenuServiceTest {
 
 		given(menuDao.findById(updateMenuDto.getId())).willReturn(getMenuInfo());
 
-		menuService.update(updateMenuDto, null);
+		menuService.update(updateMenuDto);
 
 		then(menuDao).should(times(1)).updateById(updateMenuDto);
 		then(fileService).should(times(0)).deleteFile(any());
 		then(fileService).should(times(0)).deleteFileInfo(any());
-		then(fileService).should(times(0)).uploadFile(any());
-		then(fileService).should(times(0)).saveFileInfo(any());
-	}
-
-	@Test
-	@DisplayName("기존 파일이 존재하지 않고 파일 업로드 시 성공적으로 업로드 된다.")
-	void successToUploadMenuImageIfOriginalFileNotExists() {
-		MultipartFile multipartFile = getMockMultipartFile("image", "image.jpg");
-		UpdateMenuDto updateMenuDto = getUpdateMenuDto();
-
-		given(menuDao.findById(updateMenuDto.getId())).willReturn(getMenuInfo());
-
-		menuService.update(updateMenuDto, multipartFile);
-
-		then(menuDao).should(times(1)).updateById(updateMenuDto);
-		then(fileService).should(times(0)).deleteFile(any());
-		then(fileService).should(times(0)).deleteFileInfo(any());
-		then(fileService).should(times(1)).uploadFile(multipartFile);
-		then(fileService).should(times(1)).saveFileInfo(any());
 	}
 
 	@Test
@@ -148,31 +172,26 @@ public class MenuServiceTest {
 
 		given(menuDao.findById(updateMenuDto.getId())).willReturn(menuInfo);
 
-		menuService.update(updateMenuDto, null);
+		menuService.update(updateMenuDto);
 
 		then(menuDao).should(times(1)).updateById(updateMenuDto);
 		then(fileService).should(times(1)).deleteFile(menuInfo.getFile());
 		then(fileService).should(times(1)).deleteFileInfo(menuInfo.getFile().getId());
-		then(fileService).should(times(0)).uploadFile(any());
-		then(fileService).should(times(0)).saveFileInfo(any());
 	}
 
 	@Test
-	@DisplayName("기존 메뉴 이미지 파일을 변경하면 서버에서 파일이 삭제 후 업로드 된다.")
+	@DisplayName("기존 메뉴 이미지 파일을 변경하면 서버에서 파일이 삭제된다.")
 	void successToChangeSavedFileIfUploadFileIsNotNull() {
-		MultipartFile multipartFile = getMockMultipartFile("image", "image.jpg");
 		UpdateMenuDto updateMenuDto = getUpdateMenuDtoWithFile();
 		MenuInfo menuInfo = getMenuInfoWithFile();
 
 		given(menuDao.findById(updateMenuDto.getId())).willReturn(menuInfo);
 
-		menuService.update(updateMenuDto, multipartFile);
+		menuService.update(updateMenuDto);
 
 		then(menuDao).should(times(1)).updateById(updateMenuDto);
 		then(fileService).should(times(1)).deleteFile(menuInfo.getFile());
 		then(fileService).should(times(1)).deleteFileInfo(menuInfo.getFile().getId());
-		then(fileService).should(times(1)).uploadFile(multipartFile);
-		then(fileService).should(times(1)).saveFileInfo(any());
 	}
 
 	@Test
@@ -224,7 +243,8 @@ public class MenuServiceTest {
 			.price(1000)
 			.sort(1)
 			.menuGroupId(1L)
-			.file(FileDto.builder().id(1L).build())
+			.originFile(FileDto.builder().id(1L).build())
+			.uploadFile(FileDto.builder().id(2L).build())
 			.build();
 	}
 
