8d245d22b4e5ae62674ff37c7b208a4c2f01762e
sjhello <sjhello235@gmail.com>
2021-05-20 13:30:40 +0900

    refactor: @JsonValue를 사용하지 않고 enum 상수값으로 변경

--- src/main/java/com/restaurant/eatenjoy/util/restaurant/PaymentType.java
+++ src/main/java/com/restaurant/eatenjoy/util/restaurant/PaymentType.java
@@ -1,21 +1,6 @@
 package com.restaurant.eatenjoy.util.restaurant;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum PaymentType {
 
-	PREPAYMENT("선불"),
-	POSTPAID("매장 결제"),
-	FREE("자유 결제");
-
-	private String paymentType;
-
-	@JsonValue
-	public String getPaymentType() {
-		return paymentType;
-	}
-
-	PaymentType(String paymentType) {
-		this.paymentType = paymentType;
-	}
+	PREPAYMENT, POSTPAID, FREE
 }
